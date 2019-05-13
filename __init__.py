21c06e26ea7edecca49296385c40d2d895606aca
Jack Lu <jlu9@stuy.edu>
2019-05-13 14:06:42 -0400

    worked on login/register page

--- __init__.py
+++ __init__.py
@@ -6,7 +6,11 @@ app.secret_key = os.urandom(32)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("landing.html")
+
+@app.route("/login_register")
+def login_register():
+    return render_template("login_register.html")
 
 if __name__ == "__main__":
     app.debug = True
