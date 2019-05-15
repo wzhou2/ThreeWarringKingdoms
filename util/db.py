0fd5b98d6be701e7a075261173507ad214420aee
wzhou2 <wzhou2@stuy.edu>
2019-05-15 10:39:25 -0400

    inital db.py with docstrings using sqlite3

---
+++ util/db.py
@@ -0,0 +1,118 @@
+# import sqlalchemy as db #waiting for permission to use
+import sqlite3
+class Database:
+    """A class to faciliate database read/write.
+    """
+
+    def __init__(self, file):
+        self.DB_FILE = file
+        self.conn = sqlite3.connect(file)
+        self.conn.close()
+
+    def openDB(func):
+        """A wrapper function which opens the database for read/write
+
+        Args:
+            func (obj): A function that performs an operation on the database
+
+        Returns:
+            obj: A function object
+        """
+        def inner(self, *args):
+            with sqlite3.connect(self.DB_FILE) as db:
+                return func(self, *args)
+        return inner
+
+    @openDB
+    def createTable(self, name, *cols):
+        """ Create a x column table with the name provided given it
+        does not exist yet.
+
+        Args:
+            name (str): The name of the table
+            *cols: The list of column names
+
+        Returns:
+            bool: True if successful, False otherwise
+        """
+        print(name, "ff", *cols)
+        return 1
+
+    @openDB
+    def tableInDB(self, name):
+        """ Checks if a table exists in the database
+
+        Args:
+            name (str): The name of the table
+
+        Returns:
+            bool: True if successful, False otherwise
+
+        """
+        pass
+
+    @openDB
+    def insert(self, name, values):
+        """ Inserts a row with the given values into a table with the
+        corresponding name.
+
+        Args:
+            name (str): The name of the table
+            values (array): Values to be inserted into the table
+
+        Returns:
+            bool: True if successful, False otherwise
+
+        Raises:
+            ValueError: The values param does not contain enough element to fill
+            the row.
+        """
+        pass
+
+    @openDB
+    def get(self, name, *cols):
+        """ Gets values for the columns in cols that satisfy the the conditions
+        from the a table
+
+        Args:
+            name (str): The name of the table
+            *cols: The list of column names
+            **conditions: A dictionary of conditions. The key is the column name
+            and the value is the logic condition
+
+        Returns:
+            list : a list of table rows that satisfy the conditiions
+
+        """
+        pass
+
+    def verifyUser(self, user, password):
+        """ Verifies that the user is
+        1. in the database and
+        2. the user's inputted password matches the one in the database
+
+        Args:
+            user (str): The name of the user
+            password (str): The inputted password of the user
+
+        Return:
+            bool: True if successful, False otherwise
+
+        """
+        pass
+
+    def insertUser(self, **info):
+        """ Adds a row entry into the table of users
+
+        Args:
+            **info : Dictionary of user inputted information
+
+        Returns:
+            bool: True if successful, False otherwise
+        """
+        pass
+
+if __name__=="__main__":
+    db = Database("test.db")
+    a = db.createTable("users", ["user", "first", "last", "password"])
+    print(a)
