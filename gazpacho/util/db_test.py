d88f82cbbe0a4d67dda1c2e7f5f9b5ba7d626c68
wzhou2 <wzhou2@stuy.edu>
2019-05-18 22:42:30 -0400

    temp tester for db functions need to rearrange

---
+++ gazpacho/util/db_test.py
@@ -0,0 +1,12 @@
+from db import *
+
+data = Database("test.db")
+a = data.tableInDB('d')
+# print(a)
+vals = ['cow', 'dog', 'peep', 'a']
+args = ["user TEXT", "first TEXT", "last TEXT", "password TEXT"]
+b = data.createTable("users", *args)
+c = data.insert("users", vals)
+
+d = data.get("users", "user", "password")
+e = data.checkUser("a")
