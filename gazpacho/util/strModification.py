ee1a20ab126d95d3a16c6fbd5cc5b44315f1a3d3
wzhou2 <wzhou2@stuy.edu>
2019-05-24 12:22:28 -0400

    file to manipulation of strings and lists

---
+++ gazpacho/util/strModification.py
@@ -0,0 +1,37 @@
+def stringToList(str, seperator=","):
+    """ Takes a string and converts it to a list
+
+    Args:
+        str (str): string represenation of a list
+        seperator (str): the string that seperates the values of the list
+
+    Returns:
+        list: the list represenated by str
+
+    >> stringToList("")
+    []
+    >> stringToList("0")
+    ["0"]
+    >> stringToList("0,cow")
+    ["0", "cow"]
+    """
+    pass
+
+def listToString(list, seperator=","):
+    """ Converts a list to a string
+
+    Args:
+        list (list): list of strings
+        seperator (str): the string that seperates the values of the list
+
+    Returns:
+        str: the string representation of a list
+
+    >> listToString([])
+    ""
+    >> listToString(["0"])
+    "0"
+    >> listToString(["0","cow"])
+    "0,cow"
+    """
+    pass
