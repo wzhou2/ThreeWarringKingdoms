9827cb43f911ced0640e0db18a67f655470a770f
wzhou2 <wzhou2@stuy.edu>
2019-05-24 10:51:46 -0400

    using row number for id

--- gazpacho/__init__.py
+++ gazpacho/__init__.py
@@ -14,7 +14,7 @@ print(DB_PATH)
 store = db.Database(DB_PATH)
 TABLES = {
     'users': ['user TEXT PRIMARY KEY', 'first TEXT', 'last TEXT', 'password TEXT', 'salary INTEGER', 'POSITION TEXT'],
-    'projects': ['id INTEGER', 'name TEXT PRIMARY KEY', 'creator TEXT', 'members TEXT'],
+    'projects': [ 'name TEXT PRIMARY KEY', 'creator TEXT', 'members TEXT'],
     'schedules': ['user TEXT PRIMARY KEY', 'monday TEXT', 'tuesday TEXT', 'wednesday TEXT', 'thursday TEXT', 'friday TEXT', 'saturday TEXT', 'sunday TEXT'],
     'record' : ['target TEXT', 'initated_by TEXT', 'type TEXT', 'description TEXT', 'id INTEGER', 'timeStamp INTEGER', 'message TEXT', 'view_level INTEGER']
 }
