c8fea2dc0b2d3f017fc2c4e4ecd27755a0ab6c8f
DevMMing <mming@stuy.edu>
2019-05-27 14:41:50 -0400

    added comment

--- gazpacho/__init__.py
+++ gazpacho/__init__.py
@@ -101,6 +101,9 @@ def project():
 def account():
     """ Returns the account page
     """
+    #if store.get('block.db',session['username'],salary,0):
+    #    return render_template("account.html")
+    #return render_template("account.html",editable=True)  ## not sure how db conditionals work 
     return render_template("account.html")
 
 @app.route("/task")
