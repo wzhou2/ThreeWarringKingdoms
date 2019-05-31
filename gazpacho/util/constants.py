f43535e81a199616e45043abd2da094e38c240b3
wzhou2 <wzhou2@stuy.edu>
2019-05-31 12:52:01 -0400

    added days of the week constants

--- gazpacho/util/constants.py
+++ gazpacho/util/constants.py
@@ -5,9 +5,9 @@ SCHEDULES_TABLE = "schedules"
 RECORD_TABLE = "record"
 
 TABLES = {
-    USER_TABLE: ['user TEXT PRIMARY KEY', 'first TEXT', 'last TEXT', 'password TEXT', 'salary INTEGER', 'position TEXT'],
+    USER_TABLE: ['username TEXT PRIMARY KEY', 'first TEXT', 'last TEXT', 'password TEXT', 'salary INTEGER', 'position TEXT'],
     PROJECTS_TABLE: [ 'name TEXT PRIMARY KEY', 'creator TEXT', 'members TEXT'],
-    SCHEDULES_TABLE: ['user TEXT PRIMARY KEY', 'monday TEXT', 'tuesday TEXT', 'wednesday TEXT', 'thursday TEXT', 'friday TEXT', 'saturday TEXT', 'sunday TEXT'],
+    SCHEDULES_TABLE: ['username TEXT PRIMARY KEY', 'monday TEXT', 'tuesday TEXT', 'wednesday TEXT', 'thursday TEXT', 'friday TEXT', 'saturday TEXT', 'sunday TEXT'],
     RECORD_TABLE: ['target TEXT', 'initated_by TEXT', 'type TEXT', 'description TEXT', 'id INTEGER', 'timeStamp INTEGER', 'message TEXT', 'view_level INTEGER']
 }
 
@@ -18,6 +18,15 @@ LAST = "last"
 PASSWORD = "password"
 SALARY = "salary"
 POSITION = "position"
+
+ALL = "*"
+MONDAY = "monday"
+TUESDAY = "tuesday"
+WEDNESDAY = "wednesday"
+THURSDAY = "thursday"
+FRIDAY = "friday"
+SATURDAY = "saturday"
+SUNDAY = "sunday"
 
 # ACCESS LEVEL
 ACCESS = {
