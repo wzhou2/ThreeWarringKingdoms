73830825373ca5850f774c03cf4e106974dfec7b
Jack Lu <jlu9@stuy.edu>
2019-06-05 16:26:33 -0400

    refactor db schema

--- gazpacho/util/constants.py
+++ gazpacho/util/constants.py
@@ -7,10 +7,11 @@ MESSAGE_TABLE = "messages"
 
 TABLES = {
     USER_TABLE: ['username TEXT PRIMARY KEY', 'first TEXT', 'last TEXT', 'password TEXT', 'salary INTEGER', 'position TEXT'],
-    PROJECTS_TABLE: [ 'name TEXT PRIMARY KEY', 'creator TEXT', 'members TEXT'],
+    PROJECTS_TABLE: [ 'name TEXT PRIMARY KEY', 'creator TEXT', 'members TEXT', "description TEXT"],
     SCHEDULES_TABLE: ['username TEXT PRIMARY KEY', 'monday TEXT', 'tuesday TEXT', 'wednesday TEXT', 'thursday TEXT', 'friday TEXT', 'saturday TEXT', 'sunday TEXT'],
-    RECORD_TABLE: ['target TEXT', 'initated_by TEXT', 'type TEXT', 'description TEXT', 'id INTEGER', 'timeStamp INTEGER', 'message TEXT', 'view_level INTEGER'],
-    MESSAGE_TABLE: ['sent_to TEXT', 'sent_by TEXT', 'topic TEXT', 'content TEXT', 'timeStamp INTEGER' ]
+    # RECORD_TABLE: ['target TEXT', 'initated_by TEXT', 'type TEXT', 'description TEXT', 'id INTEGER', 'timeStamp INTEGER', 'message TEXT', 'view_level INTEGER'],
+    RECORD_TABLE: ["username TEXT", "type TEXT", "timestamp INTEGER", "description TEXT"],
+    MESSAGE_TABLE: ['sent_to TEXT', 'sent_by TEXT', 'topic TEXT', 'content TEXT', 'timestamp INTEGER' ]
 }
 
 # CONSTANTS
